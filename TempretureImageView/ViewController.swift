c43f6e8a5e1c12c799d1b854ace9525c20dbf952
Mac <mac@macs-imac.local>
2020-08-14 15:00:41 +0530

    Initial Commit

---
+++ TempretureImageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TempretureImageView
+//
+//  Created by Mac on 14/08/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
