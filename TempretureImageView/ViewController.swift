7e71423e42f3a42c5a17eba1631f32671d2f03f5
Mac <mac@macs-imac.local>
2020-08-20 16:01:55 +0530

    Design is completed

--- TempretureImageView/ViewController.swift
+++ TempretureImageView/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var beautyImg: UIImageView!
 
+    @IBOutlet weak var label: UILabel!
+    
+    
+    @IBOutlet weak var label6: UILabel!
+    
+    
+    @IBOutlet weak var label4: UILabel!
+    
+    
+    @IBOutlet weak var label8: UILabel!
+    
+    @IBOutlet weak var label615: UILabel!
+    
+    @IBOutlet weak var label816: UILabel!
+    
     override func viewDidLoad() {
+       
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        label.text = " 22º "
+        label6.text = "4º/14º"
+         label8.text = "6º/12º"
+         label615.text = "7º/17º"
+         label816.text = "9º/19º"
+        
+        
+      
     }
 
 
+    
+    
 }
 
